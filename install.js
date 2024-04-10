4704459cae31540a5f13ac5e3b1cd1828cb3f2d4
cocktailpeanut <cocktailpeanuts@proton.me>
2024-04-10 19:27:52 -0400

    init

---
+++ install.js
@@ -0,0 +1,39 @@
+const config = require("./config.js")
+const pre = require("./pre.js")
+module.exports = async (kernel) => {
+  let script = {
+    run: [{
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://huggingface.co/spaces/cocktailpeanut/parler_tts_mini app",
+        ]
+      }
+    }, {
+      method: "shell.run",
+      params: {
+        venv: "env",
+        path: "app",
+        message: [
+          "pip install -r requirements.txt",
+          "pip install gradio"
+        ],
+      }
+    }, {
+      method: "fs.share",
+      params: {
+        venv: "app/env"
+      }
+    }, {
+      method: "notify",
+      params: {
+        html: "Click the 'start' tab to get started!"
+      }
+    }]
+  }
+  let pre_command = pre(config, kernel)
+  if (pre_command) {
+    script.run[1].params.message = [pre_command].concat(script.run[1].params.message)
+  }
+  return script
+}
---
+++ pinokio.js
@@ -0,0 +1,64 @@
+const path = require('path')
+module.exports = {
+  version: "1.5",
+  title: "parler-tts",
+  description: "a lightweight text-to-speech (TTS) model that can generate high-quality speech with features that can be controlled using a simple text prompt (e.g. gender, background noise, speaking rate, pitch and reverberation). https://huggingface.co/spaces/parler-tts/parler_tts_mini",
+  icon: "icon.png",
+  menu: async (kernel) => {
+    let installing = await kernel.running(__dirname, "install.js")
+    let installed = await kernel.exists(__dirname, "app", "env")
+    let running = await kernel.running(__dirname, "start.js")
+    if (installing) {
+      return [{
+        icon: "fa-solid fa-plug",
+        text: "Installing",
+        href: "install.js",
+      }]
+    } else if (installed) {
+      if (running) {
+        let local = kernel.memory.local[path.resolve(__dirname, "start.js")]
+        if (local && local.url) {
+          return [{
+            icon: "fa-solid fa-rocket",
+            text: "Open Web UI",
+            href: local.url,
+          }, {
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        } else {
+          return [{
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        }
+      } else {
+        return [{
+          icon: "fa-solid fa-power-off",
+          text: "Start",
+          href: "start.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Update",
+          href: "update.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Install",
+          href: "install.js",
+        }, {
+          icon: "fa-regular fa-circle-xmark",
+          text: "Reset",
+          href: "reset.js",
+        }]
+      }
+    } else {
+      return [{
+        icon: "fa-solid fa-plug",
+        text: "Install",
+        href: "install.js",
+      }]
+    }
+  }
+}
